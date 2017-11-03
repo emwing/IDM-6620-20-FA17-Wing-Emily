b9ba5c58b834b2895c10629dbdd01145964c1b81
emwing <emwing@emily-wing.com>
2017-11-03 03:56:01 -0400

    initial commit

---
+++ wk10/script.js
@@ -0,0 +1,5 @@
+/* function ():
+  -Clicking on an A element applies a CSS class to it that changes it’s background color */
+
+  /* function ():
+    -Clicking on one of the H2 post titles in the main content area hides it */
