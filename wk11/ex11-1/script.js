f1a03263d46c6825a36eea87ca5e49b11ad7ca77
emwing <emwing@emily-wing.com>
2017-11-07 01:33:12 -0500

    final commit of week 11 exercise 1

--- wk11/ex11-1/script.js
+++ wk11/ex11-1/script.js
@@ -10,9 +10,7 @@
     $("h2.product-name[data-type='mineralwater']").css("background-color", "#0000cd");
     $("h2.product-name[data-type='proteinbar']").css("background-color", "#9932cc");
 
-/* begin ex10-2 programming challenge code */
-/* the following solution code was written with guidance from the lynda.com solution video
-
+/* function ():
   -create event listener for when status of product checkbox changes (identifying checkbox by its id)
   -calls updateProductView function, which takes two arguements:
     -data-type
@@ -27,6 +25,23 @@
     document.querySelector("#proteinbar-cb").addEventListener('change',function (evt) {
       updateProductView("proteinbar", evt.target.checked);
     });
+
+/* begin ex11-1 programming challenge code */
+/* the following solution code was written with guidance from the lynda.com solution video */
+/* function to open and populate the product page with two query string parameters from the product that was clicked (product name and product id):
+  -get all the list item tags that contain each product
+  -uses browser's encodeURIComponenet function to convert any charactes that aren't allowed to appear in URLs
+  -create a new link tag that links to the product page and includes the two parameters inside the URL */
+
+  $(".product-item").each(function () {
+    var prodName = encodeURIComponent($(this).children("h2").text());
+    var prodID = encodeURIComponent($(this).data("prod_id"));
+
+    var link = $("<a href='product.html?prodName=" + prodName + "&prodID=" + prodID + "'/>");
+    $(this).children("img").wrap(link);
+  })
+
+/* function for the product page  to parse out the product name and product id from the url located in the script-prod.js file */
 
 /* function updateProductView ():
   -acquires a list of the product items for the associated category
