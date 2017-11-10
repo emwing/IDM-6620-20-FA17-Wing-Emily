dd6eeeaf6dce5857ee9480e19e9cbda90d98f6d5
emwing <emwing@emily-wing.com>
2017-11-09 20:22:14 -0500

    initial commit
    starting with updated files from assignment 10

---
+++ wk11/script.js
@@ -0,0 +1,19 @@
+/* function ():
+  -Clicking on an <a> applies a .bgChangeCol CSS class to it that changes it’s background color */
+
+  $(document).ready(function () {
+    $("a").click(function () {
+      $(this).toggleClass("bgChangeCol");
+    });
+
+  /* function ():
+    -Clicking on one of the H2 post titles in the main content area hides it */
+    $("h2.title").click(function () {
+      $(this).next().toggle("slow");
+    });
+
+  // begin code for assignment 11:
+
+    
+
+  }) /* ends document ready function */
