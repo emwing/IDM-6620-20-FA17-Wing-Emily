4259044a0bc0fea8a6dc0a285bec1afa65812d06
emwing <emwing@emily-wing.com>
2017-12-03 23:42:30 -0500

    final commit of week 13 assignment
    converted interface to flip card format, added additonal numbers, and
    used week 12 assignment feedback to write functions

--- wk13/script.js
+++ wk13/script.js
@@ -1,37 +1,19 @@
-/* function toggleTransle():
-  -Accepts ID of HTML elementID
-  -Listens for button click
-  -Toggle innerHTML */
+$(document).ready(function() {
 
-  $("document").ready(function() {
-    var words = document.getElementById('words'),
-      toggle = 1;
-
-    $('#translate').click(function() {
-      switch (toggle) {
-        case 0:
-          words.innerHTML = "<p>one two three</p>";
-          toggle = 1;
-          break;
-
-        case 1:
-          words.innerHTML = "<p>ichi ni san</p><p>Japanese Romanji</p>";
-          toggle = 2;
-          break;
-
-        case 2:
-          words.innerHTML = "<p>いち に さん</p><p>Japanese Hiragana</p>";
-          toggle = 3;
-          break;
-
-        case 3:
-          words.innerHTML = "<p>一 二 三</p><p>Japanese Kanji</p>";
-          toggle = 0;
-          break;
-
-        default:
-          words.innerHTML = "<p>1 2 3</p>";
-          break;
-      }
+// flip the card by adding classes with transformations
+  $(".card").click(function() {
+      $(this).addClass("frontflipped");
+      $(this).find(".front").addClass("backflipped");
+      $(this).find(".front").css("z-index", 0);
+      $(this).css("z-index", 10);
     });
+
+// reset the cards when #reset button clicked
+  $("#reset").click(function() {
+    $(".card").removeClass("frontflipped");
+    $(".card").find(".front").removeClass("backflipped");
+    $(".card").find(".front").css("z-index", "");
+    $(".card").css("z-index", "");
   });
+
+}) // ends document ready
